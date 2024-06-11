0c2f9202203ea0acc44bdf41d182edf5015c04a1
Luis de Santos <ldesa3@uic.edu>
2024-06-10 22:31:34 -0500

    Migrated code over to poise framework

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 use std::env;
 
-use songbird::SerenityInit;
-use songbird::events::{Event, EventHandler as VoiceEventHandler, TrackEvent, EventContext};
-use serenity::all::{GatewayIntents};
+use serenity::all::GatewayIntents;
 use serenity::async_trait;
+use serenity::client::Context;
 use serenity::model::channel::Message;
 use serenity::prelude::*;
-use serenity::client::Context;
+use songbird::SerenityInit;
 
+use poise::serenity_prelude;
+type FrameworkContext<'a> = poise::Framework<(), SerenityError>;
 struct Handler;
+struct General;
 
 #[async_trait]
 impl EventHandler for Handler {
@@ -17,15 +19,15 @@ impl EventHandler for Handler {
             "!ping" => {
                 msg.react(&ctx.http, '👀').await;
                 msg.channel_id.say(&ctx.http, "Pong!").await;
-            },
+            }
             "!deleteme" => {
                 eprintln!("Got a delete me");
                 let result = msg.delete(&ctx.http).await;
-                if result.is_err(){
+                if result.is_err() {
                     eprintln!("Error deleting message");
                     eprintln!("Error msg: {:?}", result.unwrap());
                 }
-            },
+            }
             "!join" => {
                 msg.react(&ctx.http, '👀').await;
                 let (guild_id, channel_id) = {
@@ -51,25 +53,26 @@ impl EventHandler for Handler {
                     .expect("Songbird Voice client placed")
                     .clone();
 
-
-
-
-                match  manager.join(guild_id, connect).await {
+                match manager.join(guild_id, connect).await {
                     Ok(_) => {
                         println!("Succesfully connected!");
-                    },
-                    Err(e) => panic!("Could not join: {:?}", e)
+                    }
+                    Err(e) => panic!("Could not join: {:?}", e),
                 }
-            },
-            _ => ()
+            }
+            _ => (),
         }
     }
+}
+
+async fn ping(ctx: Context, msg: Message) -> Result<(), SerenityError> {
+    Ok(())
 }
 
 #[tokio::main]
 async fn main() {
     println!("Starting application");
-    
+
     let token = env::var("DISCORD_TOKEN").expect("Expected discord token to be set in environment");
 
     let intents = GatewayIntents::GUILD_VOICE_STATES
@@ -78,10 +81,25 @@ async fn main() {
         | GatewayIntents::GUILD_MESSAGE_REACTIONS
         | GatewayIntents::MESSAGE_CONTENT;
 
+    let framework = poise::Framework::builder()
+        .setup(|_ctx, _ready, _: &FrameworkContext| Box::pin(async move { Ok(()) }))
+        .options(poise::FrameworkOptions {
+            skip_checks_for_owners: true,
+            prefix_options: poise::PrefixFrameworkOptions {
+                prefix: Some("!".into()),
+                case_insensitive_commands: true,
+                edit_tracker: None,
+                execute_self_messages: false,
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+        .build();
     let mut client = Client::builder(&token, intents)
         .event_handler(Handler)
         .register_songbird()
-        .await.expect("Error creating client");
+        .await
+        .expect("Error creating client");
 
     client.start().await;
 }
