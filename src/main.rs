6027653deec1d1eae542b8967d973e481dfb32b6
Luis de Santos <ldesa3@uic.edu>
2024-06-12 21:52:47 -0500

    Join command should NOT be needed args

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,7 @@ use std::env;
 
 use serenity::prelude::GatewayIntents;
 use serenity::prelude::Client;
-use serenity::prelude::Context;
 use serenity::model::channel::Message;
-use poise::serenity_prelude as serenity;
 use songbird::SerenityInit;
 use std::collections::vec_deque::VecDeque;
 
@@ -12,11 +10,50 @@ mod youtube;
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type BotContext<'a> = poise::Context<'a, VecDeque<youtube::SongMessage>, Error>;
 
-#[poise::command(prefix_command)]
+#[poise::command(
+    prefix_command,
+    user_cooldown=10,
+    aliases("check", "ustraight")
+)]
 async fn ping(ctx: BotContext<'_>) -> Result<(), Error>{
     ctx.say("Pong!").await?;
     Ok(())
+}
 
+#[poise::command(prefix_command, aliases("link"))]
+async fn join(
+    ctx: BotContext<'_>,
+    msg: Message) -> Result<(), Error>{
+    ctx.say("Joining!").await?;
+    let (guild_id, channel_id) = {
+        let guild = msg.guild(&ctx.cache()).unwrap();
+        let channel_id = guild
+            .voice_states
+            .get(&msg.author.id)
+            .and_then(|voice_state| voice_state.channel_id);
+
+        (guild.id, channel_id)
+    };
+
+    let connect = match channel_id {
+        Some(channel) => channel,
+        None => {
+            panic!("Could not match channel_id!")
+        }
+    };
+
+    let manager = songbird::get(&ctx.serenity_context())
+        .await
+        .expect("Songbird voice client err")
+        .clone();
+
+    match manager.join(guild_id, connect).await {
+        Ok(_) => {
+            eprintln!("Succesfully connected!");
+        }
+        Err(e) => panic!("Could not join: {:?}", e)
+    }
+    Ok(())
 }
 
 #[tokio::main]
@@ -33,20 +70,24 @@ async fn main() {
 
     let framework = poise::Framework::<VecDeque<youtube::SongMessage>, Error>::builder()
         .options(poise::FrameworkOptions {
-            commands: vec![ping()],
+            commands: vec![
+                ping(),
+                join()
+            ],
             skip_checks_for_owners: true,
+            manual_cooldowns: false,
             prefix_options: poise::PrefixFrameworkOptions {
                 prefix: Some("!".into()),
-                case_insensitive_commands: true,
                 edit_tracker: None,
                 execute_self_messages: false,
+                ignore_thread_creation: true,
                 ..Default::default()
             },
             ..Default::default()
         }).setup(move |ctx, _ready, framework| {
             Box::pin(async move {
                 ctx.set_presence(None, serenity::model::user::OnlineStatus::Idle);
-                let commands = poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
                 Ok(VecDeque::<youtube::SongMessage>::new())
             })
         })
